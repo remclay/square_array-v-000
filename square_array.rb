d66827ff4153e94ab288e82feb7869e6cde1cf0f
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 04:41:19 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
+new_square_array = []
   array.each do |number|
-    new_square_array = []
     new_square_array << (number ** 2)
   end
   new_square_array
