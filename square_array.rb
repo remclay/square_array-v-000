0a3702710f1bd2e86754bbd137d2dac2348c788c
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 04:52:07 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-## def square_array(array)
+def square_array(array)
   # your code here
-#new_square_array = []
-#  array.each {|number| new_square_array << (number ** 2)}
-#  new_square_array
-#end
+new_square_array = []
+  array.each {|number| new_square_array << (number ** 2)}
+  new_square_array
+end
 
 ## Implemented with .collect
 #def square_array(array)
