1b7368673d582c2bfe5030c483733cab0314b02b
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 04:38:50 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
   # your code here
-  array.each! {|number| number ** 2}
+  array.each do |number|
+    new_square_array = []
+    new_square_array << (number ** 2)
+
   end
 end
