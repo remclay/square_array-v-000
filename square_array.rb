5c036f99346e66183679ceb9bbfde613ac223cf4
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 04:49:36 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,15 @@
+## def square_array(array)
+  # your code here
+#new_square_array = []
+#  array.each {|number| new_square_array << (number ** 2)}
+#  new_square_array
+#end
+
 def square_array(array)
   # your code here
 new_square_array = []
-  array.each {|number| new_square_array << (number ** 2)}
+  array.collect {|number| number ** 2}
   new_square_array
 end
+
+
